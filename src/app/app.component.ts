d24c9fa95d5d5f5bf2e9cf3c02dad2e27b63912a
thienth32 <thienth32@gmail.com>
2019-07-10 15:59:32 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+// import {DetailComponent} from './components/detail/detail.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,23 +8,63 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'pt13311';
 
-  students = [
-  	{
-  		id: 1,
-  		name: 'Viet Anh',
-  		age: 30
-  	},{
-  		id: 2,
-  		name: 'Thuy Linh',
-  		age: 50
-  	},{
-  		id: 3,
-  		name: 'Chung',
-  		age: 10
-  	},{
-  		id: 4,
-  		name: 'Xuan Son',
-  		age: 18
-  	}
+  user = {
+    name: 'nguyen tri dien',
+    age: 20,
+    address: '15 dong quan',
+    hometown: 'ha nam'
+  }
+
+  heroList = [
+    { id: 11, name: 'Dr Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
   ];
+
+  // arrow function 
+  removeHero = (heroId) => {
+    this.heroList = this.heroList.filter(
+      (item) => item.id != heroId
+    );
+  }
+
+  keyupHeroName = (event) => {
+    if(event.keyCode === 13){
+      // tim so id lon nhat
+      let maxId = 0;
+      for (var i = 0; i < this.heroList.length; ++i) {
+        if(this.heroList[i].id >= maxId){
+          maxId = this.heroList[i].id
+        }
+      }
+
+      // tao hero moi
+      let item = {
+        name: event.target.value,
+        id: maxId + 1
+      }
+
+      // add hero moi vao mang
+      this.heroList.push(item);
+      // lam trong the input
+      event.target.value = "";
+    }
+  }
+
+
+
+
 }
+
+
+
+
+
+
