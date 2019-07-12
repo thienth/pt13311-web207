bebb6f53b7b797884b2e06f81f9351a94d8154bb
Vanh <tranvanh0905@gmail.com>
2019-07-12 16:30:07 +0700

    change Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,21 +18,47 @@ export class AppComponent {
     newHeroName = '';
 
     heroList = [
-        {id: 11, name: 'Dr Nice'},
-        {id: 12, name: 'Narco'},
-        {id: 13, name: 'Bombasto'},
-        {id: 14, name: 'Celeritas'},
-        {id: 15, name: 'Magneta'},
-        {id: 16, name: 'RubberMan'},
-        {id: 17, name: 'Dynama'},
-        {id: 18, name: 'Dr IQ'},
-        {id: 19, name: 'Magma'},
-        {id: 20, name: 'Tornado'}
+        {id: 11, name: 'Dr Nice', image: 'Đây là một cái ảnh'},
+        {id: 12, name: 'Narco', image: 'Đây là một cái ảnh'},
+        {id: 13, name: 'Bombasto', image: 'Đây là một cái ảnh'},
+        {id: 14, name: 'Celeritas', image: 'Đây là một cái ảnh'},
+        {id: 15, name: 'Magneta', image: 'Đây là một cái ảnh'},
+        {id: 16, name: 'RubberMan', image: 'Đây là một cái ảnh'},
+        {id: 17, name: 'Dynama', image: 'Đây là một cái ảnh'},
+        {id: 18, name: 'Dr IQ', image: 'Đây là một cái ảnh'},
+        {id: 19, name: 'Magma', image: 'Đây là một cái ảnh'},
+        {id: 20, name: 'Tornado', image: 'Đây là một cái ảnh'}
     ];
-    // arrow function
-    removeHero = (heroId) => {
-        // this.heroList = this.heroList.filter((item) => item.id !== heroId);
+
+    heroTmp = {
+        id: 0,
+        name: '',
+        image: '',
     }
+
+    // arrow function
+    // removeHero = (heroId) => {
+    //     console.log(this.heroList[0]);
+    //     this.heroList = this.heroList.filter((item) => item.id !== heroId);
+    // }
+
+    editHero = (hero, event) => {
+        this.heroTmp = {...hero};
+    }
+    //
+    // editHero = (heroId) => {
+    //     consl
+    // }
+
+    private getNewId = () => {
+        let maxId = 0;
+        for (var i = 0; i < this.heroList.length; i++) {
+            if (this.heroList[i].id >= maxId) {
+                maxId = this.heroList[i].id
+            }
+        }
+        return maxId +1;
+    };
     keyupHeroName = (event) => {
         if (event.keyCode === 13) {
             let maxId = 0;
@@ -44,6 +70,7 @@ export class AppComponent {
             let item = {
                 name: event.target.value,
                 id: this.heroList.length + 1,
+                image: "",
             };
             this.heroList.push(item);
             event.target.value = '';
